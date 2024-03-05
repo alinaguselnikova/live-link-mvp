ec4fc10f7e401ad9a59db9442ef081e00c86feea
alinaguselnikova <a.guselnikova@g.nsu.ru>
2024-03-05 23:04:24 +0700

    fix: useRoomOptions hook

--- pages/api/hooks/useRoomOptions.ts
+++ pages/api/hooks/useRoomOptions.ts
@@ -1,93 +1,137 @@
+import { decodePassphrase, useServerUrl } from '@/lib/client-utils';
 import { LocalUserChoices } from '@livekit/components-react';
 import {
+  DeviceUnsupportedError,
   ExternalE2EEKeyProvider,
+  Room,
+  RoomConnectOptions,
   RoomOptions,
   VideoCodec,
   VideoPresets,
 } from 'livekit-client';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function useRoomOptions(
-  e2eeEnabled: boolean,
-  hq: string | string[] | undefined,
-  codec: string | string[] | undefined,
-  userChoices: LocalUserChoices,
-  keyProvider: ExternalE2EEKeyProvider,
-  worker: false| '' |Worker
+  userChoices: LocalUserChoices
 ) {
-    let [roomOptions, setRoomOptions] = React.useState<RoomOptions>();
-    React.useEffect(() => {
-        let videoCodec: VideoCodec | undefined = (
-            Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
-          ) as VideoCodec;
-          if (e2eeEnabled && (videoCodec === 'av1' || videoCodec === 'vp9')) {
-            videoCodec = undefined;
+    const router = useRouter();
+    const { region, hq, codec } = router.query;
+  
+    const e2eePassphrase =
+      typeof window !== 'undefined' &&
+      decodePassphrase(location.hash.substring(1));
+  
+    const liveKitUrl = useServerUrl(region as string | undefined);
+  
+    const worker =
+      typeof window !== 'undefined' &&
+      e2eePassphrase &&
+      new Worker(new URL('livekit-client/e2ee-worker', import.meta.url));
+  
+    const e2eeEnabled = !!(e2eePassphrase && worker);
+    const keyProvider = new ExternalE2EEKeyProvider();
+
+  let [roomOptions, setRoomOptions] = React.useState<RoomOptions>();
+  React.useEffect(() => {
+    let videoCodec: VideoCodec | undefined = (
+      Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
+    ) as VideoCodec;
+    if (e2eeEnabled && (videoCodec === 'av1' || videoCodec === 'vp9')) {
+      videoCodec = undefined;
+    }
+    setRoomOptions({
+      videoCaptureDefaults: {
+        deviceId: userChoices.videoDeviceId ?? undefined,
+        resolution: hq === 'true' ? VideoPresets.h2160 : VideoPresets.h720,
+      },
+      publishDefaults: {
+        dtx: false,
+        videoSimulcastLayers:
+          hq === 'true'
+            ? [VideoPresets.h1080, VideoPresets.h720]
+            : [VideoPresets.h540, VideoPresets.h216],
+        red: !e2eeEnabled,
+        videoCodec,
+      },
+      audioCaptureDefaults: {
+        deviceId: userChoices.audioDeviceId ?? undefined,
+      },
+      adaptiveStream: { pixelDensity: 'screen' },
+      dynacast: true,
+      e2ee: e2eeEnabled
+        ? {
+            keyProvider,
+            worker,
           }
-          setRoomOptions ( {
-            videoCaptureDefaults: {
-              deviceId: userChoices.videoDeviceId ?? undefined,
-              resolution: hq === 'true' ? VideoPresets.h2160 : VideoPresets.h720,
-            },
-            publishDefaults: {
-              dtx: false,
-              videoSimulcastLayers:
-                hq === 'true'
-                  ? [VideoPresets.h1080, VideoPresets.h720]
-                  : [VideoPresets.h540, VideoPresets.h216],
-              red: !e2eeEnabled,
-              videoCodec,
-            },
-            audioCaptureDefaults: {
-              deviceId: userChoices.audioDeviceId ?? undefined,
-            },
-            adaptiveStream: { pixelDensity: 'screen' },
-            dynacast: true,
-            e2ee: e2eeEnabled
-              ? {
-                  keyProvider,
-                  worker,
-                }
-              : undefined,
-          // I think you'll kill me for this
-          } as RoomOptions);
-        },[userChoices, hq, codec]);
-    return roomOptions;
-//   
+        : undefined,
+      // I think you'll kill me for this
+    } as RoomOptions);
+  }, [userChoices, hq, codec]);
 
+  let [room, setRoom] = React.useState<Room>();
+  React.useEffect(() => {
+    setRoom(new Room(roomOptions));
+  }, []);
 
-//const roomOptions = React.useMemo((): RoomOptions => {
-//     let videoCodec: VideoCodec | undefined = (
-//       Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
-//     ) as VideoCodec;
-//     if (e2eeEnabled && (videoCodec === 'av1' || videoCodec === 'vp9')) {
-//       videoCodec = undefined;
-//     }
-//     return {
-//       videoCaptureDefaults: {
-//         deviceId: userChoices.videoDeviceId ?? undefined,
-//         resolution: hq === 'true' ? VideoPresets.h2160 : VideoPresets.h720,
-//       },
-//       publishDefaults: {
-//         dtx: false,
-//         videoSimulcastLayers:
-//           hq === 'true'
-//             ? [VideoPresets.h1080, VideoPresets.h720]
-//             : [VideoPresets.h540, VideoPresets.h216],
-//         red: !e2eeEnabled,
-//         videoCodec,
-//       },
-//       audioCaptureDefaults: {
-//         deviceId: userChoices.audioDeviceId ?? undefined,
-//       },
-//       adaptiveStream: { pixelDensity: 'screen' },
-//       dynacast: true,
-//       e2ee: e2eeEnabled
-//         ? {
-//             keyProvider,
-//             worker,
-//           }
-//         : undefined,
-//     // I think you'll kill me for this
-//     } as RoomOptions;
-//   }, [userChoices, hq, codec]);
+  if (e2eeEnabled) {
+    keyProvider.setKey(decodePassphrase(e2eePassphrase));
+    room!.setE2EEEnabled(true).catch((e) => {
+      if (e instanceof DeviceUnsupportedError) {
+        alert(
+          `You're trying to join an encrypted meeting, but your browser does not support it. Please update it to the latest version and try again.`,
+        );
+        console.error(e);
+      }
+    });
+  }
+
+  const connectOptions = React.useMemo((): RoomConnectOptions => {
+    return {
+      autoSubscribe: true,
+    };
+  }, []);
+
+  return( {
+    liveKitUrl,
+    room,
+    connectOptions
+  });
+  //
+
+  //const roomOptions = React.useMemo((): RoomOptions => {
+  //     let videoCodec: VideoCodec | undefined = (
+  //       Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
+  //     ) as VideoCodec;
+  //     if (e2eeEnabled && (videoCodec === 'av1' || videoCodec === 'vp9')) {
+  //       videoCodec = undefined;
+  //     }
+  //     return {
+  //       videoCaptureDefaults: {
+  //         deviceId: userChoices.videoDeviceId ?? undefined,
+  //         resolution: hq === 'true' ? VideoPresets.h2160 : VideoPresets.h720,
+  //       },
+  //       publishDefaults: {
+  //         dtx: false,
+  //         videoSimulcastLayers:
+  //           hq === 'true'
+  //             ? [VideoPresets.h1080, VideoPresets.h720]
+  //             : [VideoPresets.h540, VideoPresets.h216],
+  //         red: !e2eeEnabled,
+  //         videoCodec,
+  //       },
+  //       audioCaptureDefaults: {
+  //         deviceId: userChoices.audioDeviceId ?? undefined,
+  //       },
+  //       adaptiveStream: { pixelDensity: 'screen' },
+  //       dynacast: true,
+  //       e2ee: e2eeEnabled
+  //         ? {
+  //             keyProvider,
+  //             worker,
+  //           }
+  //         : undefined,
+  //     // I think you'll kill me for this
+  //     } as RoomOptions;
+  //   }, [userChoices, hq, codec]);
 }
--- pages/rooms/[name].tsx
+++ pages/rooms/[name].tsx
@@ -19,7 +19,7 @@ import {
   VideoPreset,
   VideoPresets,
 } from 'livekit-client';
-// import useRoomOptions from '../api/hooks/useRoomOptions';
+import useRoomOptions from '../api/hooks/useRoomOptions';
 
 export default function EnterPage() {
   const router = useRouter();
@@ -110,65 +110,61 @@ const ActiveRoom = ({ roomName, userChoices, onLeave }: ActiveRoomProps) => {
     tokenOptions,
   );
 
-  const router = useRouter();
-  const { region, hq, codec } = router.query;
+  // const router = useRouter();
+  // const { region, hq, codec } = router.query;
 
-  const e2eePassphrase =
-    typeof window !== 'undefined' &&
-    decodePassphrase(location.hash.substring(1));
+  // const e2eePassphrase =
+  //   typeof window !== 'undefined' &&
+  //   decodePassphrase(location.hash.substring(1));
 
-  const liveKitUrl = useServerUrl(region as string | undefined);
+  // const liveKitUrl = useServerUrl(region as string | undefined);
 
-  const worker =
-    typeof window !== 'undefined' &&
-    e2eePassphrase &&
-    new Worker(new URL('livekit-client/e2ee-worker', import.meta.url));
+  // const worker =
+  //   typeof window !== 'undefined' &&
+  //   e2eePassphrase &&
+  //   new Worker(new URL('livekit-client/e2ee-worker', import.meta.url));
 
-  const e2eeEnabled = !!(e2eePassphrase && worker);
-  const keyProvider = new ExternalE2EEKeyProvider();
+  // const e2eeEnabled = !!(e2eePassphrase && worker);
+  // const keyProvider = new ExternalE2EEKeyProvider();
 
-  // const roomOptions = useRoomOptions(e2eeEnabled, hq,
-  //   codec,
-  //   userChoices,
-  //   keyProvider,
-  //   worker)
+  const roomOptions = useRoomOptions(userChoices)
   /** FIXME: unnecessary useMemo, move all room building logic into hook plz ^_^ */
-  let [roomOptions, setRoomOptions] = React.useState<RoomOptions>();
-  React.useEffect(() => {
-    let videoCodec: VideoCodec | undefined = (
-      Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
-    ) as VideoCodec;
-    if (e2eeEnabled && (videoCodec === 'av1' || videoCodec === 'vp9')) {
-      videoCodec = undefined;
-    }
-    setRoomOptions({
-      videoCaptureDefaults: {
-        deviceId: userChoices.videoDeviceId ?? undefined,
-        resolution: hq === 'true' ? VideoPresets.h2160 : VideoPresets.h720,
-      },
-      publishDefaults: {
-        dtx: false,
-        videoSimulcastLayers:
-          hq === 'true'
-            ? [VideoPresets.h1080, VideoPresets.h720]
-            : [VideoPresets.h540, VideoPresets.h216],
-        red: !e2eeEnabled,
-        videoCodec,
-      },
-      audioCaptureDefaults: {
-        deviceId: userChoices.audioDeviceId ?? undefined,
-      },
-      adaptiveStream: { pixelDensity: 'screen' },
-      dynacast: true,
-      e2ee: e2eeEnabled
-        ? {
-            keyProvider,
-            worker,
-          }
-        : undefined,
-    }
-    );
-  }, [userChoices, hq, codec]);
+  // let [roomOptions, setRoomOptions] = React.useState<RoomOptions>();
+  // React.useEffect(() => {
+  //   let videoCodec: VideoCodec | undefined = (
+  //     Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
+  //   ) as VideoCodec;
+  //   if (e2eeEnabled && (videoCodec === 'av1' || videoCodec === 'vp9')) {
+  //     videoCodec = undefined;
+  //   }
+  //   setRoomOptions({
+  //     videoCaptureDefaults: {
+  //       deviceId: userChoices.videoDeviceId ?? undefined,
+  //       resolution: hq === 'true' ? VideoPresets.h2160 : VideoPresets.h720,
+  //     },
+  //     publishDefaults: {
+  //       dtx: false,
+  //       videoSimulcastLayers:
+  //         hq === 'true'
+  //           ? [VideoPresets.h1080, VideoPresets.h720]
+  //           : [VideoPresets.h540, VideoPresets.h216],
+  //       red: !e2eeEnabled,
+  //       videoCodec,
+  //     },
+  //     audioCaptureDefaults: {
+  //       deviceId: userChoices.audioDeviceId ?? undefined,
+  //     },
+  //     adaptiveStream: { pixelDensity: 'screen' },
+  //     dynacast: true,
+  //     e2ee: e2eeEnabled
+  //       ? {
+  //           keyProvider,
+  //           worker,
+  //         }
+  //       : undefined,
+  //   }
+  //   );
+  // }, [userChoices, hq, codec]);
   // const roomOptions = React.useMemo((): RoomOptions => {
   //   let videoCodec: VideoCodec | undefined = (
   //     Array.isArray(codec) ? codec[0] : codec ?? 'vp9'
@@ -206,37 +202,37 @@ const ActiveRoom = ({ roomName, userChoices, onLeave }: ActiveRoomProps) => {
 
   /** FIXME: instead useMemo use useState+useEffect w/ empty deps array (move into hook) */
   // const room = React.useMemo(() => new Room(roomOptions), []);
-  let [room, setRoom] = React.useState<Room>();
-  React.useEffect(() => {
-    setRoom(new Room(roomOptions));
-  }, []);
+  // let [room, setRoom] = React.useState<Room>();
+  // React.useEffect(() => {
+  //   setRoom(new Room(roomOptions));
+  // }, []);
 
-  if (e2eeEnabled) {
-    keyProvider.setKey(decodePassphrase(e2eePassphrase));
-    room!.setE2EEEnabled(true).catch((e) => {
-      if (e instanceof DeviceUnsupportedError) {
-        alert(
-          `You're trying to join an encrypted meeting, but your browser does not support it. Please update it to the latest version and try again.`,
-        );
-        console.error(e);
-      }
-    });
-  }
+  // if (e2eeEnabled) {
+  //   keyProvider.setKey(decodePassphrase(e2eePassphrase));
+  //   room!.setE2EEEnabled(true).catch((e) => {
+  //     if (e instanceof DeviceUnsupportedError) {
+  //       alert(
+  //         `You're trying to join an encrypted meeting, but your browser does not support it. Please update it to the latest version and try again.`,
+  //       );
+  //       console.error(e);
+  //     }
+  //   });
+  // }
 
-  const connectOptions = React.useMemo((): RoomConnectOptions => {
-    return {
-      autoSubscribe: true,
-    };
-  }, []);
+  // const connectOptions = React.useMemo((): RoomConnectOptions => {
+  //   return {
+  //     autoSubscribe: true,
+  //   };
+  // }, []);
 
   return (
     <>
-      {liveKitUrl && (
+      {roomOptions.liveKitUrl && (
         <LiveKitRoom
-          room={room}
+          room={roomOptions.room}
           token={token}
-          serverUrl={liveKitUrl}
-          connectOptions={connectOptions}
+          serverUrl={roomOptions.liveKitUrl}
+          connectOptions={roomOptions.connectOptions}
           video={userChoices.videoEnabled}
           audio={userChoices.audioEnabled}
           onDisconnected={onLeave}
